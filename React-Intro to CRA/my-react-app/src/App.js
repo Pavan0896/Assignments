bacd2d1f1fe6187f33850dc2e981f9b413d54135
Pavan0896 <pavankumar170896@gmail.com>
2024-04-07 11:23:55 +0530

    intro to CRA

---
+++ React-Intro to CRA/my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { React, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <h1>Counter : {count}</h1>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+
+export default App;
